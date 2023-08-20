6823004bce2efa14f005149f6fa649733bef214e
Yahya <yahyakamran099@gmail.com>
2023-08-21 02:36:07 +0500

    Configured the database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
+  try { 
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log(`MongoDB Connected : ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error : ${error.message}`);
+    console.log(`Error is in DB : ${error.message}`);
     process.exit();
   }
 };
