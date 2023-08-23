0e28a76276815b75ee61809be25ab43e61c7dea2
Yahya <yahyakamran099@gmail.com>
2023-08-24 01:14:30 +0500

    Configured HomePage

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,27 +1,24 @@
-import React, { useEffect, useState } from "react";
-import axios from "../http";
+import { ChatState } from "../Context/ChatProvider";
+import { Box } from "@chakra-ui/react";
+import SideDrawer from "../components/Miscellaneous/SideDrawer";
+import MyChats from "../components/MyChats";
+import ChatBox from "../components/ChatBox";
 
 function ChatPage() {
-  const [chats, setChats] = useState([]);
-
-  const fetchChats = async () => {
-    try {
-      const { data } = await axios.get("/api/chats");
-      setChats(data);
-    } catch (error) {
-      console.log(error.response.data);
-    }
-  };
-
-  useEffect(() => {
-    fetchChats();
-  }, []);
-
+  const { user } = ChatState();
   return (
-    <div>
-      {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
-      ))}
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        width={"100%"}
+        height={"91.5vh"}
+        padding={"10px "}
+      >
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
     </div>
   );
 }
